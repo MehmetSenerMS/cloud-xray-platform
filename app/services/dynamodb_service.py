dbc86d3e7ba94cfd1e950df3d7a20ed247047b08
MehmetSener <ms.mese@outlook.com>
2026-05-25 23:51:32 +0200

    Fix DynamoDB float issue

--- app/services/dynamodb_service.py
+++ app/services/dynamodb_service.py
@@ -1,6 +1,7 @@
 import boto3
 from boto3.dynamodb.conditions import Key
-
+from decimal import Decimal
+import json
 
 TABLE_NAME = "XRayTransactions"
 REGION_NAME = "us-east-1"
@@ -14,9 +15,15 @@ table = dynamodb.Table(TABLE_NAME)
 
 
 def save_transaction_to_dynamodb(transaction_record: dict):
-    table.put_item(Item=transaction_record)
-    return transaction_record
 
+    transaction_record = json.loads(
+        json.dumps(transaction_record),
+        parse_float=Decimal
+    )
+
+    table.put_item(Item=transaction_record)
+
+    return transaction_record
 
 def get_transactions_by_user(user_id: str):
     response = table.query(
